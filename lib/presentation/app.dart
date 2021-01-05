8faff9cd501bad79d85b24946a788fd2f382d421
Mark Eliasen <hello@sirmre.com>
2021-01-05 22:45:14 +0000

    move app into presentation

---
+++ lib/presentation/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_boilerplate/presentation/app_theme.dart';
+import 'package:flutter_app_boilerplate/presentation/widgets/splash_screen/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'presentation/routes/routes.dart';
+
+class App extends StatelessWidget {
+  final GlobalKey<NavigatorState> navKey;
+  const App(this.navKey);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        // BlocProvider<BlocA>(
+        //   create: (BuildContext context) => BlocA(),
+        // ),
+        // BlocProvider<BlocB>(
+        //   create: (BuildContext context) => BlocB(),
+        // ),
+        // BlocProvider<BlocC>(
+        //   create: (BuildContext context) => BlocC(),
+        // ),
+      ],
+      child: MaterialApp(
+        title: 'Boilerplate',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: AppTheme.swatch,
+        ),
+        initialRoute: SplashScreen.routeName,
+        onGenerateRoute: (RouteSettings settings) {
+          // return the route if found
+          if (routes.containsKey(settings.name)) {
+            return routes[settings.name](settings) as Route<dynamic>;
+          }
+
+          // otherwise return "404" error page
+          return routes['/unknown-route']() as Route<dynamic>;
+        },
+        navigatorKey: navKey,
+      ),
+    );
+  }
+}
