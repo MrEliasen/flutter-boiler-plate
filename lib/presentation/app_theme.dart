9d3f7f203f382cc0e6668e8f11423cc3721f7c9d
Mark Eliasen <hello@sirmre.com>
2021-01-05 22:45:29 +0000

    update default app theme

--- lib/presentation/app_theme.dart
+++ lib/presentation/app_theme.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const MaterialColor swatch = Colors.pink;
-  static const Color primary = Colors.pink;
-  static const Color primaryBackground = Color(0xFFF3F4F7);
-  static const Color primaryFontColour = Color(0xFF333D55);
+  /// main material app colour swatch
+  static const MaterialColor swatch = Colors.indigo;
+
+  /// primary and secondary app colour
+  static const Color primaryColour = Color(0xff2a628f);
+  static const Color secondaryColour = Color(0xff3e92cc);
+
+  ///primary and secondary background colour
+  static const Color primaryBackgroundColour = Color(0xffF5F5F6);
+  static const Color secondaryBackgroundColour = Color(0xffE1E2E1);
+
+  ///primary and secondary font colour
+  static const Color primaryFontColour = Colors.white;
+  static const Color secondaryFontColour = Color(0xff2E3842);
 }
